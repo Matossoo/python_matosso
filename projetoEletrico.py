ce29d19a47140f9365b96d3e99e4e77fadd2759e
matosso <rafael_matosso@estudante.sesisenai.org.br>
2026-02-25 20:18:07 -0300

    melhorado

--- projetoEletrico.py
+++ projetoEletrico.py
@@ -1,49 +1,23 @@
-print("=== CALCULADORA ELÉTRICA ===")
+import webbrowser
 
-print("\nEscolha o cálculo:")
-print("1 - Calcular Tensão (V)")
-print("2 - Calcular Corrente (I)")
-print("3 - Calcular Resistência (R)")
-print("4 - Calcular Potência (P)")
-print("5 - Calcular Consumo (kWh)")
+while True:
+    print("\n=== MENU NAVEGADOR ===")
+    print("1 - Abrir Google")
+    print("2 - Abrir YouTube")
+    print("3 - Abrir site personalizado")
+    print("0 - Sair")
 
-opcao = input("Digite a opção: ")
+    opcao = input("Escolha uma opção: ")
 
-if opcao == "1":
-    r = float(input("Digite a resistência (Ohms): "))
-    i = float(input("Digite a corrente (Amperes): "))
-    v = r * i
-    print("Tensão =", v, "Volts")
-
-elif opcao == "2":
-    v = float(input("Digite a tensão (Volts): "))
-    r = float(input("Digite a resistência (Ohms): "))
-    if r != 0:
-        i = v / r
-        print("Corrente =", i, "Amperes")
+    if opcao == "1":
+        webbrowser.open("https://www.google.com")
+    elif opcao == "2":
+        webbrowser.open("https://www.youtube.com")
+    elif opcao == "3":
+        site = input("Digite o link completo: ")
+        webbrowser.open(site)
+    elif opcao == "0":
+        print("Encerrando programa...")
+        break
     else:
-        print("Erro: resistência não pode ser zero!")
-
-elif opcao == "3":
-    v = float(input("Digite a tensão (Volts): "))
-    i = float(input("Digite a corrente (Amperes): "))
-    if i != 0:
-        r = v / i
-        print("Resistência =", r, "Ohms")
-    else:
-        print("Erro: corrente não pode ser zero!")
-
-elif opcao == "4":
-    v = float(input("Digite a tensão (Volts): "))
-    i = float(input("Digite a corrente (Amperes): "))
-    p = v * i
-    print("Potência =", p, "Watts")
-
-elif opcao == "5":
-    p = float(input("Digite a potência do aparelho (Watts): "))
-    t = float(input("Digite o tempo de uso (horas): "))
-    consumo = (p * t) / 1000
-    print("Consumo =", consumo, "kWh")
-
-else:
-    print("Opção inválida!")
+        print("Opção inválida!")
